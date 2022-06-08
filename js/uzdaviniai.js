f12fd0e0c29a480a75e497cd93f323827da7e325
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-06-08 12:57:32 +0300

    uzd

---
+++ js/uzdaviniai.js
@@ -0,0 +1,20 @@
+/* 1. Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console */
+
+const a = 5;
+console.log(a);
+const b = 8;
+console.log(b);
+const c = 3;
+console.log(c);
+
+/* 2. Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console */
+
+const username = 'Jonas';
+console.log(username);
+const miestas = 'Vilnius';
+console.log(miestas);
+const augintinis = 'kate';
+console.log(augintinis);
+
