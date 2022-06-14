f5a8886d057e0ad55ebb9ebf46d0a3bd0b8ce8a7
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-06-14 15:40:08 +0300

    uzd

---
+++ js/other-loops.js
@@ -0,0 +1,114 @@
+const marks = [10, 2, 8, 4, 6];
+
+// for
+console.log('----- for');
+for (let i = 0; i < marks.length; i++) {
+    const mark = marks[i];
+    console.log(mark);
+}
+
+// for-of
+console.log('----- for-of');
+for (const mark of marks) {
+    console.log(mark);
+}
+
+// while
+console.log('----- while');
+let i = 0;
+while (i < marks.length) {
+    const mark = marks[i];
+    console.log(mark);
+    i++;
+}
+
+// do-while
+console.log('----- do-while');
+let i2 = 0;
+do {
+    const mark = marks[i2];
+    console.log(mark);
+    i2++;
+} while (i2 < marks.length)
+
+// foreach
+console.log('----- foreach');
+marks.forEach(mark => {
+    console.log(mark);
+});
+
+// map - kaip modifikuoti kiekviena nari
+console.log('----- map');
+const rezMap = marks
+    .map(mark => mark * 2)
+    .map(mark => mark > 10 ? 10 : mark);
+
+const rezMap2 = marks.map(mark => mark * 2 > 10 ? 10 : mark * 2);
+
+console.log(marks);
+console.log(rezMap);
+console.log(rezMap2);
+
+// map - kaip modifikuoti kiekviena nari
+console.log('----- filter');
+const filterMarks = marks
+    .filter(mark => mark > 2)
+    .filter(mark => mark < 7);
+
+const filterMarks2 = marks.filter(mark => mark > 2 && mark < 7);
+
+console.log(marks);
+console.log(filterMarks);
+console.log(filterMarks2);
+
+// map & filter combo
+console.log('----- map-filter');
+const mapFilter = marks
+    .map(mark => mark * 2)
+    .filter(mark => mark > 5);
+
+console.log(marks);
+console.log(mapFilter);
+
+// reduce (subendrinti)
+console.log('----- reduce');
+const sumReduce = marks.reduce((total, mark) => total + mark);
+const sumReduce20 = marks.reduce((total, mark) => total + mark, 20);
+console.log(sumReduce);
+console.log(sumReduce20);
+
+const difReduce = marks.reduce((total, mark) => total - mark);
+const difReduce20 = marks.reduce((total, mark) => total - mark, 20);
+console.log(difReduce);
+console.log(difReduce20);
+
+const multiReduce = marks.reduce((total, mark) => total * mark);
+const multiReduce20 = marks.reduce((total, mark) => total * mark, 20);
+console.log(multiReduce);
+console.log(multiReduce20);
+
+const divReduce = marks.reduce((total, mark) => total / mark);
+const divReduce20 = marks.reduce((total, mark) => total / mark, 20);
+console.log(divReduce);
+console.log(divReduce20);
+
+// sort
+console.log('----- sort:numbers');
+const numList = [10, 2, 8, 4, 6, 1, 7, 23, 17];
+const minMaxList = [...numList].sort((a, b) => a - b);
+const maxMinList = [...numList].sort((a, b) => b - a);
+
+console.log(numList);
+console.log(minMaxList);
+console.log(maxMinList);
+
+console.log('----- sort:strings');
+const stringList = ['b', 'd', 'c', 'de', 'be', 'a', 'at'];
+const stringsSort = [...stringList].sort();
+const minMaxStrings = [...stringList].sort((a, b) => a > b ? -1 : 1);
+const maxMinStrings = [...stringList].sort((a, b) => a < b ? -1 : 1);
+
+console.log(stringList);
+console.log(stringsSort);
+console.log(minMaxStrings);
+console.log(maxMinStrings);
--- js/uzdaviniai.js
+++ js/uzdaviniai.js
@@ -330,24 +330,79 @@ Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra 4 viene
 Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra 3 vienetai.
 Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra 2 vienetai. */
 
-let cikl11 = '';
+console.log('----------------------------');
 let sk11 = '';
+let sk12 = '';
+let sk13 = '';
 let sum11 = '';
+let sum12 = '';
+let sum13 = '';
 for (let i = 0; i <= 11; i++) {
     if (i % 3 === 0) {
         sk11 += [i];
         sum11 = sk11.toString();
-        console.log('Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra ' + sum11.length + ' vienetai.');
-    } /*else if (i % 5 === 0) {
-        console.log(i + 'dalijasi is 5');
+    } else if (i % 5 === 0) {
+        sk12 += [i];
+        sum12 = sk12.toString();
     } else if (i % 7 === 0) {
-        console.log(i + 'dalijasi is 7');
-    } */
+        sk13 += [i];
+        sum13 = sk13.toString();
+    }
+        
 }
+console.log('Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 3 yra ' + sum11.length + ' vienetai.');
+console.log('Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 5 yra ' + sum12.length + ' vienetai.');
+console.log('Skaičių intervale tarp 0 ir 11, besidalijančių be liekanos iš 7 yra ' + sum13.length + ' vienetai.');
 
+console.log('----------------------------');
 
-/* const sk = Math.ceil(Math.log10(g + 1));
-            return sk; */
+let sk21 = '';
+let sk22 = '';
+let sk23 = '';
+let sum21 = '';
+let sum22 = '';
+let sum23 = '';
+for (let i = 8; i <= 31; i++) {
+    if (i % 3 === 0) {
+        sk21 += [i];
+        sum21 = sk21.toString();
+    } else if (i % 5 === 0) {
+        sk22 += [i];
+        sum22 = sk22.toString();
+    } else if (i % 7 === 0) {
+        sk23 += [i];
+        sum23 = sk23.toString();
+    }
+        
+}
+console.log('Skaičių intervale tarp 8 ir 31, besidalijančių be liekanos iš 3 yra ' + sum21.length + ' vienetai.');
+console.log('Skaičių intervale tarp 8 ir 31, besidalijančių be liekanos iš 5 yra ' + sum22.length + ' vienetai.');
+console.log('Skaičių intervale tarp 8 ir 31, besidalijančių be liekanos iš 7 yra ' + sum23.length + ' vienetai.');
+
+console.log('----------------------------');
+
+let sk31 = '';
+let sk32 = '';
+let sk33 = '';
+let sum31 = '';
+let sum32 = '';
+let sum33 = '';
+for (let i = -18; i <= 18; i++) {
+    if (i % 3 === 0) {
+        sk31 += [i];
+        sum31 = sk31.toString();
+    } else if (i % 5 === 0) {
+        sk32 += [i];
+        sum32 = sk32.toString();
+    } else if (i % 7 === 0) {
+        sk33 += [i];
+        sum33 = sk33.toString();
+    }
+        
+}
+console.log('Skaičių intervale tarp -18 ir 18, besidalijančių be liekanos iš 3 yra ' + sum31.length + ' vienetai.');
+console.log('Skaičių intervale tarp -18 ir 18, besidalijančių be liekanos iš 5 yra ' + sum32.length + ' vienetai.');
+console.log('Skaičių intervale tarp -18 ir 18, besidalijančių be liekanos iš 7 yra ' + sum33.length + ' vienetai.');
 
 console.log('----------------------------');
 
@@ -468,4 +523,45 @@ console.log( didziausiasSkaiciusSarase( “pomidoras” ) );
 rezultatas: “Pateikta netinkamo tipo reikšmė.”
 console.log( didziausiasSkaiciusSarase( [] ) );
 rezultatas: “Pateiktas sąrašas negali būti tuščias.” */
+
+console.log('-------------------------------------------');
+
+console.log('---------codewars-------------------------');
+
+const arr = [5, 2, 's', -3, 2, 1];
+function positiveSum(arr) {
+    let sum2 = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number' && arr[i]>0) {
+            sum2 += arr[i];
+        } 
+    } 
+    return sum2; 
+  }
+console.log(positiveSum(arr));
+
+console.log('--------------------');
+const a1 = [4, 5, 6];
+const b1 = [1, 2, 3];
+
+function arrayMadness(a1, b1) {
+    let suma1 = 0;
+    for (let i = 0; i < a1.length; i++) {
+        suma1 += a1[i] ** 2;
+    };
+    console.log(suma1);
+    let sumb1 = 0;
+    for (let i = 0; i < b1.length; i++) {
+        sumb1 += b1[i] ** 3;
+    };
+    console.log(sumb1);
+    if (suma1 > sumb1) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+console.log(arrayMadness(a1, b1)); 
+
+console.log('--------------------');
 
