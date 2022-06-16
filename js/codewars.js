5315d56ceaab17763bf9267496730944327adce7
VilmaPetruskeviciene <105633730+VilmaPetruskeviciene@users.noreply.github.com>
2022-06-16 15:16:46 +0300

    uzd

--- js/codewars.js
+++ js/codewars.js
@@ -280,13 +280,10 @@ const arrayOfSheep = [true,  true,  true,  false,
                       false, false, true,  true];
 
 function countSheeps(arrayOfSheep) {
-    let sheep = 0;
-    for (let i = 0; i <= arrayOfSheep.length; i++) {
-      if (i === true) {
-        sheep += arrayOfSheep[i];
-        
-      }   
-    } 
-    return sheep;
+  const sheep = arrayOfSheep.filter(value => value === true).length; 
+  return sheep;  
 }
 console.log(countSheeps(arrayOfSheep));
+
+console.log('--------------------');
+
--- js/number-parsing.js
+++ js/number-parsing.js
@@ -1,4 +1,4 @@
-function suma(a, b) {
+/*function suma(a, b) {
     const initialAsize = ('' + a).length;
     const initialBsize = ('' + b).length;
 
@@ -16,6 +16,21 @@ function suma(a, b) {
     }
 
     return a + b;
+}*/
+
+function suma(a, b) {
+    a = +a;
+    b = +b;
+
+    if (isNaN(a)) {
+        return 'ERROR: Pirmas parametras yra ne tikras skaicius';
+    }
+    if (isNaN(b)) {
+        return 'ERROR: Antras parametras yra ne tikras skaicius';
+    }
+
+    return a + b;
 }
+
 
 module.exports = suma;
